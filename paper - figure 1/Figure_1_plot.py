f8d9bab3aad8063659f0f17a4226dc7c42311239
nacerEB <nacer.boukacem@mail.mcgill.ca>
2023-08-28 19:24:54 -0400

    Working version for Figure 1, some fixes and optimization left namely on dir struct, also removed __init__.py in this and preciding figures as it was useless.

---
+++ paper - figure 1/Figure_1_plot.py
@@ -0,0 +1,201 @@
+import sys
+sys.path.append('../')
+
+import numpy as np
+
+import matplotlib
+font = {'family' : 'Times New Roman',
+        'weight' : 'normal',
+        'size'   : 18}
+matplotlib.rc('font', **font)
+
+import matplotlib.pyplot as plt
+import matplotlib
+
+import pickle
+import umap
+
+from main_module.KrotovV2_utils import *
+
+
+
+data_dir = "data_100_10_200/"
+run = 0
+n, temp = 30, 670
+saving_dir = data_dir+"momentum_run_"+str(run)+"_n"+str(n)+"_T"+str(temp)+".npz"
+
+# Loading data - will improve dir struct soon..
+data_M = np.load(saving_dir)['M']
+data_L = np.load(saving_dir)['L']
+data_T = np.load(data_dir+"miniBatchs_images.npy")[0]
+mapper = pickle.load((open(data_dir+"/umap_model_correlation.sav", 'rb')))
+
+embedding = mapper.transform(data_T)
+M_embedding = np.load(data_dir+"/memory_umap_embed_correlation_n"+str(n)+"_momentum.npy")
+    
+
+indices = np.zeros(20)
+for i in range(len(indices)):
+    all_indices = np.argwhere(np.argmax(data_L[-1], axis=-1) == i//2)
+    indices[i] =  all_indices[np.random.randint(len(all_indices))] # -> Pick randomly when Label is mostly # digit class i//2 $
+indices = np.asarray(indices, dtype=int)
+
+t_s = [20, 90, 125, 153, 180, 280, 344] # Handpicked
+t_samples = np.asarray(t_s)*10
+
+
+props = dict(boxstyle='round', facecolor='whitesmoke', alpha=0.5)
+
+def memory_sample_plot(ax):
+    tmax = len(data_M)
+            
+    for t_i, t in enumerate(t_samples):
+        im = ax[t_i].imshow(merge_data(data_M[t, indices], 4, 5), cmap="bwr", vmin=-1, vmax=1, aspect=5.0/4.0) # Plotting the selected memory samples 
+        ax[t_i].set_title(r"$t=$"+str(t), pad=15, fontsize=16, bbox=props) # Time stamps / Cosmetics
+        ax[t_i].axis('off')
+
+    
+
+        
+def UMAP_plot(ax):
+    M = len(data_T)
+    keys = np.zeros((M))
+    for d in range(0, 10):
+        keys[d*M//10:(d+1)*M//10] = d
+    
+    tmax, N_mem = np.shape(M_embedding)[0], np.shape(M_embedding)[1]
+        
+    for t_i in range(0, len(t_s)-1):
+        im = ax[t_i].scatter(embedding[:, 0], embedding[:, 1], c=keys, cmap="tab10", s=10, marker="*") # Plotting the UMAP training data
+    
+        for i in range(0, N_mem-1):
+            data_pnts = M_embedding[t_s[0]:t_s[t_i+1], i, :]
+            ax[t_i].plot(data_pnts[:, 0], data_pnts[:, 1], linewidth=1, alpha=0.3, color="k") # Plotting the trajectories of the memories on UMAP
+
+            # Labels / cosmetics
+            ax[0].set_ylabel("UMAP 2"); ax[3].set_ylabel("UMAP 2"); ax[3].set_xlabel("UMAP 1"); ax[4].set_xlabel("UMAP 1"); ax[5].set_xlabel("UMAP 1")
+
+        # Time stamps / Cosmetics
+        ax[t_i].text(0.95, 0.95, r"$t=$"+str(t_s[t_i+1]*10), transform=ax[t_i].transAxes, fontsize=16, verticalalignment='top', ha='right', bbox=props)
+
+        # Plotting the memories as white points for each time point.
+        ax[t_i].plot(M_embedding[t_s[t_i+1], :, 0], M_embedding[t_s[t_i+1], :, 1], marker="o",
+                               linestyle="", alpha=1, markeredgecolor="k", markerfacecolor="white", markeredgewidth=1, markersize=4)
+
+        # Cosmetics
+        if t_i != 0 and t_i != 3:
+            ax[t_i].set_yticks([])
+            
+        if t_i < 3:
+            ax[t_i].set_xticks([])
+                           
+
+        
+fig = plt.figure(figsize=(17, 14.5))
+axs = fig.subplot_mosaic("""
+
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+11111111111111111111111111112222222222222222222222222222333333333333333333333333333300
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+44444444444444444444444444445555555555555555555555555555666666666666666666666666666600
+......................................................................................
+......................................................................................
+......................................................................................
+......................................................................................
+......................................................................................
+......................................................................................
+......................................................................................
+......................................................................................
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+AAAAAAAAAAAABBBBBBBBBBBBCCCCCCCCCCCCDDDDDDDDDDDDEEEEEEEEEEEEFFFFFFFFFFFFGGGGGGGGGGGGXX
+
+""")
+
+
+ax_UMAPs = [axs['1'], axs['2'], axs['3'], axs['4'], axs['5'], axs['6']]
+ax_UMAPs = np.asarray(ax_UMAPs)          
+UMAP_plot(ax_UMAPs)
+
+
+ax_mem_sample = [axs['A'], axs['B'], axs['C'], axs['D'], axs['E'], axs['F'], axs['G']]
+ax_mem_sample = np.asarray(ax_mem_sample)
+memory_sample_plot(ax_mem_sample)
+
+
+
+ax_cb_UMAP = axs['0']
+tab10_cmap = matplotlib.cm.tab10
+tab10_norm = matplotlib.colors.Normalize(vmin=0, vmax=10)
+cb_UMAP = matplotlib.colorbar.ColorbarBase(ax_cb_UMAP, cmap=tab10_cmap, norm=tab10_norm, orientation='vertical')
+cb_UMAP.set_ticks(np.arange(0, 9, 1), minor=True)
+cb_UMAP.set_ticklabels(np.arange(0, 9, 1), minor=True)
+cb_UMAP.set_label("Digit class")
+
+
+ax_cb_mem = axs['X']
+bwr_cmap = matplotlib.cm.bwr
+bwr_norm = matplotlib.colors.Normalize(vmin=-1, vmax=1)
+cb_mem = matplotlib.colorbar.ColorbarBase(ax_cb_mem, cmap=bwr_cmap, norm=bwr_norm, orientation='vertical')
+cb_mem.set_label("Pixel value")
+
+
+from matplotlib.lines import Line2D
+custom_lines = [Line2D([0], [0], color=plt.cm.tab10(0.9), marker="*", linestyle="", ms=13),
+                Line2D([0], [0], marker="o", markeredgecolor="k", markerfacecolor="white", markeredgewidth=4, linestyle="", ms=13),
+                Line2D([0], [0], color="k", lw=2, marker="", linestyle="-")]
+
+fig.legend(custom_lines, ['Training Data', 'Memory (Visible layer)', 'Memory trajectory/trail'], loc='upper center', ncol=4)
+
+
+plt.subplots_adjust(top=0.92, wspace=1.4, hspace=1.4)
+plt.savefig("tmp_fig1.png")
+
--- paper - figure 1/main_gen.py
+++ paper - figure 1/main_gen.py
@@ -5,8 +5,7 @@ import numpy as np
 
 from main_module.KrotovV2 import *
 
-data_dir = "data_100_10_200/"#"data_222_identical/"#"data_10t/"#"data_222_pop_dynamics_uncontrolled/"#"data_20_10_20/"
-#data_dir = "data_10M_10D_10E_small_noise/"
+data_dir = "data_100_10_200/"
 
 # Makes sure the data_dir exits else creates it.
 if not path.exists(data_dir):
@@ -14,32 +13,30 @@ if not path.exists(data_dir):
     os.mkdir(data_dir)
     print(data_dir, "Created!")
 
+isFirstRun = False
+prefix = "" # Can be "reproduced_"
 
-for noise_r in [8]:#range(9, 11):
-    for temp in [670]:#[400, 550, 670]:#[800, 850]:
-        for n in [30]:#[3, 15, 30]:#[5, 10, 30]:#np.arange(2, 32, 1):
+for noise_r in [8]:
+    for temp in [670]:
+        for n in [3, 15, 30, 40]:
             print(n, temp)
             
             r = 1.0/10**(noise_r)
-            net = KrotovNet(Kx=10, Ky=10, n_deg=n, m_deg=n, M=200, nbMiniBatchs=1, momentum=0*0.6, rate=0.005, temp=temp, rand_init_mean=-0.001, rand_init_std=r)#, selected_digits=[4, 9])
+            net = KrotovNet(Kx=10, Ky=10, n_deg=n, m_deg=n, M=200, nbMiniBatchs=1, momentum=0*0.6, rate=0.005, temp=temp, rand_init_mean=-0.001, rand_init_std=r)
             
             # First run
-            #np.save(data_dir+"miniBatchs_images.npy", net.miniBatchs_images); exit()
+            if isFirstRun:
+                np.save(data_dir+"miniBatchs_images.npy", net.miniBatchs_images); exit()
             
             net.miniBatchs_images = np.load(data_dir+"miniBatchs_images.npy")
 
-            """
-            for i in range(0, net.K):                
-                net.visibleDetectors[i, :] = ( (0.1-r) * np.mean(net.miniBatchs_images[0, net.M//10:2*net.M//10, :], axis=0)
-                                              - r*np.random.randn(784))
-                
-                net.hiddenDetectors[i, :] = -0.03 + r*np.random.randn(len(net.selected_digits))
-
-            """
-
-
+            
             run = 0
-            net.train_plot_update(3500, isPlotting=False, isSaving=True, saving_dir=data_dir+"reproduced_run_"+str(run)+"_n"+str(n)+"_T"+str(temp)+".npz", testFreq=100)
+
+            if len(net.selected_digits) < 10:
+                run = net.selected_digits # Essentially if this run contains only 1, 4, 7 you specify it in the file name
+            
+            net.train_plot_update(3500, isPlotting=False, isSaving=True, saving_dir=data_dir+prefix+"run_"+str(run)+"_n"+str(n)+"_T"+str(temp)+".npz", testFreq=100)
 
  
 
