7f3824337c58f0601a817e7bd0feaee9aba6c721
nacerEB <nacer.boukacem@mail.mcgill.ca>
2023-09-15 10:59:46 -0400

    Save of MutualInformation / merge with entropy

--- MutualInformation/main_gen.py
+++ MutualInformation/main_gen.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from main_module.KrotovV2 import *
 
+
 data_dir = "data/"
 
 # Makes sure the data_dir exits else creates it.
@@ -18,15 +19,22 @@ isFirstRun = False
 selected_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]#
 prefix = str(selected_digits)+"/" # I used main,and momentum #"main"#
 
-n_range = np.arange(1, 41, 2)
+job_number = int(sys.argv[1])
 
-for noise_r in [3]:
-    for temp in [750]:#[800]:
+n_full_range = np.arange(1, 41, 1)
+
+job_max = 4
+
+n_range = n_full_range[job_number-1::job_max]
+
+   
+for noise_r in [5]:
+    for temp in [650]:
         for n in n_range:
             print(n, temp)
             
             r = 1.0/10**(noise_r)
-            net = KrotovNet(Kx=10, Ky=10, n_deg=n, m_deg=n, M=20*len(selected_digits), nbMiniBatchs=1, momentum=0*0.6, rate=0.005, temp=temp, rand_init_mean=-0.001, rand_init_std=r, selected_digits=selected_digits)
+            net = KrotovNet(Kx=10, Ky=10, n_deg=n, m_deg=n, M=20*len(selected_digits), nbMiniBatchs=1, momentum=0*0.6, rate=0.005, temp=temp, rand_init_mean=-0.003, rand_init_std=r, selected_digits=selected_digits)
             
             
             # Makes sure the data_dir exits else creates it.
@@ -44,9 +52,7 @@ for noise_r in [3]:
                     "The digits included in training are " + str(net.selected_digits) + ".\n" +\
                     "Specifics can be checked by opening the file with KV_window..."
 
-                details_file.write(details)
-                
-
+                details_file.write(details)               
 
             
            # First run
@@ -63,7 +69,8 @@ for noise_r in [3]:
 
             net.miniBatchs_images[0] = data_T[train_mask]
             
-            net.train_plot_update(7000, isPlotting=False, isSaving=True, saving_dir=data_dir+prefix+"trained_net_n"+str(n)+"_T"+str(temp)+".npz", testFreq=100)
+            #net.train_plot_update(10000, isPlotting=False, isSaving=True, saving_dir=data_dir+prefix+"trained_net_n"+str(n)+"_T"+str(temp)+".npz", testFreq=100)
+            net.train_plot_update(1, isPlotting=False, isSaving=True, saving_dir=data_dir+prefix+"trained_net_end_n"+str(n)+"_T"+str(temp)+".npz", testFreq=100)
 
             
 
