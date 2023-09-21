855626df4e3c1a6fef67f977e2a8ee844c9e126c
nacerEB <nacer.boukacem@mail.mcgill.ca>
2023-09-20 22:31:54 -0400

    Figure 4 new version with slices

--- paper - figure 4/Figure_4.py
+++ paper - figure 4/Figure_4.py
@@ -51,7 +51,7 @@ aspect = (np.min(n_range) - np.max(n_range))/(np.min(temp_range) - np.max(temp_r
 extent = [np.min(n_range), np.max(n_range), np.max(temp_range), np.min(temp_range)]
 
   
-fig = plt.figure(figsize=(18+4+1, 6+3+11))
+fig = plt.figure(figsize=(18+4+1, 10+3+6+3+11))
 axs = fig.subplot_mosaic("""
 
 111111..222222..333333x
@@ -74,6 +74,19 @@ AAAAAAAAAAABBBBBBBBBBBX
 AAAAAAAAAAABBBBBBBBBBBX
 AAAAAAAAAAABBBBBBBBBBBX
 AAAAAAAAAAABBBBBBBBBBBX
+.......................
+.......................
+.......................
+DDDDDDDDDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDDDDDDDDD
 
 """)
 
@@ -81,11 +94,19 @@ AAAAAAAAAAABBBBBBBBBBBX
 ax = np.asarray([axs['A'], axs['B']])
 
 # Plotting memory samples and their coefficients
-ax[0].imshow(merge_data(data_Ms[::2, ::2, 0, :].reshape(len(n_range[::2])*len(temp_range[::2]), 784), len(n_range[::2]), len(temp_range[::2])  ),
+ax[0].imshow(merge_data(data_Ms[::4, ::4, 0, :].reshape(len(n_range[::4])*len(temp_range[::4]), 784), len(n_range[::4]), len(temp_range[::4])  ),
              cmap="bwr", vmin=-1, vmax=1, extent=extent, aspect=aspect)
     
 ax[1].imshow(data_coefs[:, :, 0, 1], cmap="Reds", vmin=0.5, vmax=1, extent=extent, aspect=aspect) #
 
+# Plotting slices
+axs['D'].plot(data_coefs[-2, :, 0, 1], marker=".", color="red", label="T = "+str(temp_range[-2]))
+axs['D'].plot(data_coefs[-3, :, 0, 1], marker=".", color="orange", label="T = "+str(temp_range[-3]))
+axs['D'].plot(data_coefs[-4, :, 0, 1], marker=".", color="blue", label="T = "+str(temp_range[-4]))
+axs['D'].set_xlabel(r"$n$-power")
+axs['D'].set_ylabel(r"$\alpha_{4, 1}$")
+axs['D'].legend()
+             
 # Plotting the theoretical prediction (see appendix)
 n = np.arange(np.min(n_range), np.max(n_range), 0.01)
 T_calc = (data_T[0]@data_T[0] + data_T[0]@data_T[1])/( 2 * ( np.arctanh( 1 - (1.0/2.0)**(1.0/(2.0*n)) ) )**(1.0/n) )
@@ -127,9 +148,9 @@ cb_intra.set_label(r"$\alpha_{4, 1}$")
 # Panel labels
 
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-rx = [1.0, 6.0/11.0]
-ry = [1.0, 6.0/11.0]
-for i, char in enumerate(['1', 'A']):
+rx = [1.0, 6.0/11.0, 6.0/23.0]
+ry = [1.0, 6.0/11.0, 6.0/10.0]
+for i, char in enumerate(['1', 'A', 'D']):
     axs[char].text(-0.2*rx[i], 1.0+0.1*ry[i], alphabet[i], transform=axs[char].transAxes, fontsize=63, verticalalignment='bottom', ha='right', fontfamily='Times New Roman', fontweight='bold')
 
 
