b6b175d1759997f94d384e694e604cc873629a88
nacerEB <nacer.boukacem@mail.mcgill.ca>
2023-11-24 18:58:39 -0500

    Adding new version of the splitting dynamics with better formulation

--- Figure 5S.Splitting/main_new.py
+++ Figure 5S.Splitting/main_new.py
@@ -1,181 +1,131 @@
+import argparse
 import sys
 sys.path.append('../')
 
 from main_module.KrotovV2 import *
-from nullcline_gather.GatherNullClines import *
+from splittingDynamics import *
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
 
-def calc_nabla_A(delta_alpha, delta_ell, n, T, alpha, ell, pm):
-    beta = 1 - alpha
 
-    M_dot_A = (alpha*A_dot_A + beta*A_dot_B)/T
+data_dir = "data/"
 
-    d_M_dot_A_d_alpha = (A_dot_A - A_dot_B)/T
-    
-    l_A_o_A = np.tanh(ell * ( M_dot_A)**n )
-    l_gamma_o_A = -np.tanh(( M_dot_A)**n )
 
-    nabla_A_pm = ( (1 - l_A_o_A)**(2*n - 1)  *  (1 - l_A_o_A**2)  *  M_dot_A**(n-1)  *  ell
+dataset = "../defaults/miniBatchs_images_Fig_5.npy"
+data_T = np.load(dataset)[0]
+data_T_inv = np.linalg.pinv(data_T)
 
-                   + 4 * (1 + l_gamma_o_A)**(2*n-1)  *  (1 - l_gamma_o_A**2)  *  M_dot_A**(n-1)
+selected_digits = [1, 4]
 
-                   + pm * (n-1) * (1 - l_A_o_A)**(2*n - 1)  *  (1 - l_A_o_A**2)  *  M_dot_A**(n-2) * d_M_dot_A_d_alpha * ell * delta_alpha
+A, B = data_T
+A_dot_A, A_dot_B, B_dot_B = A@A, A@B, B@B
 
-                   + pm * 4 * (n-1) * (1 + l_gamma_o_A)**(2*n - 1)  *  (1 - l_gamma_o_A**2)  *  M_dot_A**(n-2) * d_M_dot_A_d_alpha * delta_alpha
 
-                   + pm * (1 - l_A_o_A)**(2*n - 1)  *  (1 - l_A_o_A**2)  *  M_dot_A**(n-1) * delta_ell
-    )
+# Initial conditions
+n = 12
+temp = 800
 
-    return nabla_A_pm
-    
+da, dl = 0.031, -0.2 # Initial perturbation
+max_epoch = 700
 
+parser = argparse.ArgumentParser(description="This program runs the splitting simulations.")
+parser.add_argument('--n', help="The hyperparameter n, the power on the dot product. [DEFAULT=12]", default=12, type=int)
+parser.add_argument('--temp', help="The hyperparameter T, the 'renormalizes' the dot product. [DEFAULT=800]", default=800, type=int)
+parser.add_argument('--da', help="The initial perturbation on the memories. [DEFAULT=0.031]", default=0.031, type=float)
+parser.add_argument('--dl', help="The initial perturbation on the labels. [DEFAULT=-0.2]", default=-0.2, type=float)
+parser.add_argument('--maxepoch', help="The maxmimum epoch for which to plot and run the 2 splitting dynamics. [DEFAULT=700]", default=700, type=int)
+parse_args = parser.parse_args()
 
-def calc_nabla_B(delta_alpha, delta_ell, n, T, alpha, ell, pm):
-    beta = 1 - alpha
+n, temp, da, dl, max_epoch = parse_args.n, parse_args.temp, parse_args.da, parse_args.dl, parse_args.maxepoch
 
-    M_dot_B = (alpha*A_dot_B + beta*B_dot_B)/T
+T_tilde = temp/(2.0**(1.0/n))
 
-    d_M_dot_B_d_alpha = (A_dot_B - B_dot_B)/T
-    
-    l_A_o_B = np.tanh(ell * ( M_dot_B)**n )
-    l_gamma_o_B = -np.tanh(( M_dot_B)**n )
+fig, ax = plt.subplots(1, 2, figsize=(16, 9))
 
-    nabla_B_pm = ( - (1 + l_A_o_B)**(2*n - 1)  *  (1 - l_A_o_B**2)  *  M_dot_B**(n-1)  *  ell
 
-                   + 4 * (1 + l_gamma_o_B)**(2*n-1)  *  (1 - l_gamma_o_B**2)  *  M_dot_B**(n-1)
+############# --- SIMULATIONS ---- ####################
 
-                   - pm * (n-1) * (1 - l_A_o_B)**(2*n - 1)  *  (1 - l_A_o_B**2)  *  M_dot_B**(n-2) * d_M_dot_B_d_alpha * ell * delta_alpha
+# 1-memory sys
+net_K1 = KrotovNet(Kx=1, Ky=1, n_deg=n, m_deg=n, M=2, nbMiniBatchs=1, momentum=0*0.6, rate=0.001, temp=T_tilde, rand_init_mean=-0.03, rand_init_std=0.03, selected_digits=[1, 4])
+net_K1.miniBatchs_images[0] = data_T
+net_K1.visibleDetectors[:] = 0.6*A #A
+net_K1.hiddenDetectors[:, :] = -1
+net_K1.hiddenDetectors[:, selected_digits[0]] = 1
 
-                   + pm * 4 * (n-1) * (1 + l_gamma_o_B)**(2*n - 1)  *  (1 - l_gamma_o_B**2)  *  M_dot_B**(n-2) * d_M_dot_B_d_alpha * delta_alpha
+net_K1.train_plot_update(4000, isPlotting=False, isSaving=True, saving_dir=data_dir+"K1.npz")
 
-                   - pm * (1 + l_A_o_B)**(2*n - 1)  *  (1 - l_A_o_B**2)  *  M_dot_B**(n-1) * delta_ell
-    )
+ell, alpha = net_K1.hiddenDetectors[0, selected_digits[0]], net_K1.visibleDetectors[0]@data_T_inv[:, 0]
 
-    return nabla_B_pm
-    
-    
+# Plotting the fixed point
+ax[0].scatter(ell, alpha, s=20, color="k", alpha=1)
 
-def calc_nabla_ell(delta_alpha, delta_ell, n, T, alpha, ell, pm):
-    beta = 1 - alpha
 
-    M_dot_A = (alpha*A_dot_A + beta*A_dot_B)/T
-    M_dot_B = (alpha*A_dot_B + beta*B_dot_B)/T
 
-    
-    d_M_dot_A_d_alpha = (A_dot_A - A_dot_B)/T
-    d_M_dot_B_d_alpha = (A_dot_B - B_dot_B)/T
+# 2-memory sys
+net_K2 = KrotovNet(Kx=2, Ky=1, n_deg=n, m_deg=n, M=2, nbMiniBatchs=1, momentum=0, rate=0.001, temp=temp, rand_init_mean=-0.03, rand_init_std=0.03, selected_digits=[1, 4])
+net_K2.miniBatchs_images[0] = data_T
 
-        
-    l_A_o_A = np.tanh(ell * ( M_dot_A)**n )
-    l_gamma_o_A = -np.tanh(( M_dot_A)**n )
-    
-    l_A_o_B = np.tanh(ell * ( M_dot_B)**n )
-    l_gamma_o_B = -np.tanh(( M_dot_B)**n )
+net_K2.visibleDetectors[0, :] = (alpha + da)*A + (1-alpha - da)*B
+net_K2.visibleDetectors[1, :] = (alpha - da)*A + (1-alpha + da)*B
 
+net_K2.hiddenDetectors[:, :] = -1
 
-    nabla_ell_pm = ( (1 - l_A_o_A)**(2*n - 1)   *   (1 - l_A_o_A**2) * M_dot_A**n
+net_K2.hiddenDetectors[0, selected_digits[0]] = ell + dl
+net_K2.hiddenDetectors[0, selected_digits[1]] = -ell - dl
 
-                     - (1 + l_A_o_B)**(2*n - 1)   *   (1 - l_A_o_B**2) * M_dot_B**n
+net_K2.hiddenDetectors[1, selected_digits[0]] = ell - dl
+net_K2.hiddenDetectors[1, selected_digits[1]] = -ell + dl
 
-                     + pm * n * (1 - l_A_o_A)**(2*n - 1)   *   (1 - l_A_o_A**2) * M_dot_A**(n-1) * d_M_dot_A_d_alpha * delta_alpha
+net_K2.train_plot_update(max_epoch, isPlotting=False, isSaving=True, saving_dir=data_dir+"K2.npz")
 
-                     - pm * n * (1 + l_A_o_B)**(2*n - 1)   *   (1 - l_A_o_B**2) * M_dot_B**(n-1) * d_M_dot_B_d_alpha * delta_alpha
-        
-    )
 
-    return nabla_ell_pm
+# Loading saved data and plotting
+data = np.load(data_dir+"K2.npz")
 
+data_M = data['M']
+data_L = data['L']
 
-def calc_nabla_gamma(n, T, alpha, ell):
-    beta = 1 - alpha
+alphas = data_M @ data_T_inv[:, 0]
+ells = data_L[:, :, selected_digits[0]]
 
-    M_dot_A = (alpha*A_dot_A + beta*A_dot_B)/T
-    M_dot_B = (alpha*A_dot_B + beta*B_dot_B)/T
-    
-    l_gamma_o_A = -np.tanh(( M_dot_A)**n )
-    l_gamma_o_B = -np.tanh(( M_dot_B)**n )
+colors = ['k', 'red']
 
+for m_i in range(2):
+    ax[0].plot(ells[:, m_i], alphas[:, m_i], linewidth=3, color=colors[m_i], alpha=0.5)
+    ax[0].scatter(ells[-1, m_i], alphas[-1, m_i], s=10, color=colors[m_i])
 
-    nabla_gamma = ( - (1 + l_gamma_o_A)**(2*n - 1)  *  (1 - l_gamma_o_A**2) * M_dot_A**n
-                    - (1 + l_gamma_o_B)**(2*n - 1)  *  (1 - l_gamma_o_B**2) * M_dot_B**n
-    )
+ax[1].plot((ells[:, 0] - ells[:, 1])/2.0, (alphas[:, 0] - alphas[:, 1])/2.0, linewidth=3, color="green", alpha=0.5)
+ax[1].scatter((ells[-1, 0] - ells[-1, 1])/2.0, (alphas[-1, 0] - alphas[-1, 1])/2.0, s=20, color="green")
 
 
-    return nabla_gamma
 
+######### ------- ANALYTICAL / NUMERICAL ---------- ##########
 
 
+# The delta alpha and delta ell meshgrid
+delta_range = np.linspace(-0.1, 0.1, 1000)
+delta_ell, delta_alpha = np.meshgrid(delta_range, delta_range) # Annoyingly the order matters one the LFS
+sd = splittingDynamics(delta_alpha, delta_ell, n, T_tilde, alpha, ell, A_dot_A, A_dot_B, B_dot_B)
+ax[1].streamplot(delta_ell, delta_alpha, sd.d_delta_ell_dt, sd.d_delta_alpha_dt, color=(0, 0, 0, 0.2), density=3)
+ax[1].set_xlim(-0.1, 0.1)
+ax[1].set_ylim(-0.1, 0.1)
 
-def calc_d_alpha_pm_delta_alpha_dt(delta_alpha, delta_ell, n, T, alpha, ell, pm):
-    nabla_A_pm = calc_nabla_A(delta_alpha, delta_ell, n, T, alpha, ell, pm)
-    nabla_B_pm = calc_nabla_B(delta_alpha, delta_ell, n, T, alpha, ell, pm)
 
-    nabla_A = calc_nabla_A(delta_alpha, delta_ell, n, T, alpha, ell, 0)
-    nabla_B = calc_nabla_B(delta_alpha, delta_ell, n, T, alpha, ell, 0)
-        
-    d_alpha_pm_delta_alpha_dt = (  nabla_A_pm - alpha*(nabla_A_pm + nabla_B_pm) - pm * delta_alpha * (nabla_A + nabla_B)  ) / ( np.abs(nabla_A_pm) + np.abs(nabla_B_pm) )
-
-    return d_alpha_pm_delta_alpha_dt
-
-
-def calc_d_ell_pm_delta_ell_dt(delta_alpha, delta_ell, n, T, alpha, ell, pm):
-
-    nabla_ell_pm = calc_nabla_ell(delta_alpha, delta_ell, n, T, alpha, ell, pm)
-    nabla_gamma = calc_nabla_gamma(n, T, alpha, ell)
-    
-    d_ell_pm_delta_ell_dt = nabla_ell_pm / np.maximum( np.abs(nabla_ell_pm), np.abs(nabla_gamma) )
-
-    return d_ell_pm_delta_ell_dt
-    
-
-ell, alpha = 0.038, 0.715
-
-delta_alpha_1d = np.linspace(0, 1, 500)
-delta_ell_1d = np.linspace(-0.5, 0.5, 500)
-ell, alpha = np.meshgrid(delta_ell_1d, delta_alpha_1d)
-
-delta_alpha, delta_ell = 0, 0
-
-
-A_dot_A, A_dot_B, B_dot_B = 753, 494, 719
-n = 15
-T = 670/(2.0**(1.0/n))
-
-
-d_alpha_p_delta_alpha_dt = calc_d_alpha_pm_delta_alpha_dt(delta_alpha, delta_ell, n, T, alpha, ell, 0)
-d_alpha_m_delta_alpha_dt = calc_d_alpha_pm_delta_alpha_dt(delta_alpha, delta_ell, n, T, alpha, ell, 0) 
-
-d_delta_alpha_dt = (d_alpha_p_delta_alpha_dt + 0*d_alpha_m_delta_alpha_dt)/1.0
-
-
-d_ell_p_delta_ell_dt = calc_d_ell_pm_delta_ell_dt(delta_alpha, delta_ell, n, T, alpha, ell, 0)
-d_ell_m_delta_ell_dt = calc_d_ell_pm_delta_ell_dt(delta_alpha, delta_ell, n, T, alpha, ell, 0)
-
-d_delta_ell_dt = (d_ell_p_delta_ell_dt + 0*d_ell_m_delta_ell_dt)/1.0
-
-
-
-fig, ax = plt.subplots(1, 1, figsize=(16, 9))
-
-
-ax.streamplot(ell, alpha, d_delta_ell_dt, d_delta_alpha_dt, color=(0, 0, 0, 0.2), density=3)
-
-
-
-
+# Plotting the nullclines
 sample_range = np.linspace(0, 1, 1000)
 l_0_mesh, alpha_mesh = np.meshgrid(sample_range*2-1, sample_range)
 
-GNC = GatherNullClines(A_dot_A, A_dot_B, B_dot_B, n, T, +1)
-ax.contour(l_0_mesh, alpha_mesh, GNC.alpha_nullcline(alpha_mesh, l_0_mesh), [0], colors="purple", linewidths=7, alpha=0.5)
-ax.contour(l_0_mesh, alpha_mesh, GNC.l_0_nullcline(alpha_mesh, l_0_mesh), [0], colors="orange", linewidths=7, alpha=0.5)
+GNC = GatherNullClines(A_dot_A, A_dot_B, B_dot_B, n, T_tilde, +1)
+print(n)
+ax[0].contour(l_0_mesh, alpha_mesh, GNC.alpha_nullcline(alpha_mesh, l_0_mesh), [0], colors="purple", linewidths=7, alpha=0.5)
+ax[0].contour(l_0_mesh, alpha_mesh, GNC.l_0_nullcline(alpha_mesh, l_0_mesh), [0], colors="orange", linewidths=7, alpha=0.5)
 
 
 
 
 plt.show()
+
 
---
+++ Figure 5S.Splitting/splittingDynamics.py
@@ -0,0 +1,188 @@
+import sys
+sys.path.append('../')
+
+from main_module.KrotovV2 import *
+from nullcline_gather.GatherNullClines import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+class splittingDynamics:
+    def __init__(self, delta_alpha, delta_ell, n, T, alpha, ell, A_dot_A, A_dot_B, B_dot_B):
+        self.delta_alpha, self.delta_ell, self.n, self.T, self.alpha, self.ell = delta_alpha, delta_ell, n, T, alpha, ell
+
+        self.A_dot_A, self.A_dot_B, self.B_dot_B = A_dot_A, A_dot_B, B_dot_B
+
+
+        # Getting quantities d(alpha +- delta_alpha)/dt and d(ell +- delta_ell)/dt
+        self.d_alpha_p_delta_alpha_dt = self.calc_d_alpha_pm_delta_alpha_dt(+1)
+        self.d_alpha_m_delta_alpha_dt = self.calc_d_alpha_pm_delta_alpha_dt(-1)
+
+        self.d_ell_p_delta_ell_dt = self.calc_d_ell_pm_delta_ell_dt(+1)
+        self.d_ell_m_delta_ell_dt = self.calc_d_ell_pm_delta_ell_dt(-1)
+
+
+        # Getting d(alpha)/dt, d(ell)/dt as well as d(delta_alpha)/dt and d(delta_ell)/dt
+        self.d_alpha_dt = ( self.d_alpha_p_delta_alpha_dt + self.d_alpha_m_delta_alpha_dt  )/2.0
+        self.d_ell_dt = ( self.d_ell_p_delta_ell_dt + self.d_ell_m_delta_ell_dt  )/2.0
+
+        self.d_delta_alpha_dt = ( self.d_alpha_p_delta_alpha_dt - self.d_alpha_m_delta_alpha_dt  )/2.0
+        self.d_delta_ell_dt = ( self.d_ell_p_delta_ell_dt - self.d_ell_m_delta_ell_dt  )/2.0
+        
+        
+    def calc_nabla_A(self, pm):
+
+        # Fetching class data
+        delta_alpha, delta_ell, n, T, alpha, ell = self.delta_alpha, self.delta_ell, self.n, self.T, self.alpha, self.ell
+        A_dot_A, A_dot_B, B_dot_B = self.A_dot_A, self.A_dot_B, self.B_dot_B
+        
+        beta = 1 - alpha
+        
+        M_dot_A = (alpha*A_dot_A + beta*A_dot_B)
+        
+        d_M_dot_A_d_alpha = (A_dot_A - A_dot_B)
+        
+        l_A_o_A = np.tanh(ell * ( M_dot_A/T)**n )
+        l_gamma_o_A = -np.tanh(( M_dot_A/T)**n )
+        
+        nabla_A_pm = ( (1 - l_A_o_A)**(2*n - 1)  *  (1 - l_A_o_A**2)  *  M_dot_A**(n-1)  *  ell
+                       
+                       + 4 * (1 + l_gamma_o_A)**(2*n-1)  *  (1 - l_gamma_o_A**2)  *  M_dot_A**(n-1)
+                       
+                       + pm * (n-1) * (1 - l_A_o_A)**(2*n - 1)  *  (1 - l_A_o_A**2)  *  M_dot_A**(n-2) * d_M_dot_A_d_alpha * ell * delta_alpha
+                       
+                       + pm * 4 * (n-1) * (1 + l_gamma_o_A)**(2*n - 1)  *  (1 - l_gamma_o_A**2)  *  M_dot_A**(n-2) * d_M_dot_A_d_alpha * delta_alpha
+                       
+                       + pm * (1 - l_A_o_A)**(2*n - 1)  *  (1 - l_A_o_A**2)  *  M_dot_A**(n-1) * delta_ell
+        )
+
+        return nabla_A_pm
+    
+
+
+    def calc_nabla_B(self, pm):
+
+        # Fetching class data
+        delta_alpha, delta_ell, n, T, alpha, ell = self.delta_alpha, self.delta_ell, self.n, self.T, self.alpha, self.ell
+        A_dot_A, A_dot_B, B_dot_B = self.A_dot_A, self.A_dot_B, self.B_dot_B
+        
+        beta = 1 - alpha
+        
+        M_dot_B = (alpha*A_dot_B + beta*B_dot_B)
+        
+        d_M_dot_B_d_alpha = (A_dot_B - B_dot_B)
+        
+        l_A_o_B = np.tanh(ell * ( M_dot_B/T)**n )
+        l_gamma_o_B = -np.tanh(( M_dot_B/T)**n )
+        
+        nabla_B_pm = ( - (1 + l_A_o_B)**(2*n - 1)  *  (1 - l_A_o_B**2)  *  M_dot_B**(n-1)  *  ell
+                       
+                       + 4 * (1 + l_gamma_o_B)**(2*n-1)  *  (1 - l_gamma_o_B**2)  *  M_dot_B**(n-1)
+                       
+                       - pm * (n-1) * (1 - l_A_o_B)**(2*n - 1)  *  (1 - l_A_o_B**2)  *  M_dot_B**(n-2) * d_M_dot_B_d_alpha * ell * delta_alpha
+                       
+                       + pm * 4 * (n-1) * (1 + l_gamma_o_B)**(2*n - 1)  *  (1 - l_gamma_o_B**2)  *  M_dot_B**(n-2) * d_M_dot_B_d_alpha * delta_alpha
+                       
+                       - pm * (1 + l_A_o_B)**(2*n - 1)  *  (1 - l_A_o_B**2)  *  M_dot_B**(n-1) * delta_ell
+        )
+        
+        return nabla_B_pm
+    
+    
+
+    def calc_nabla_ell(self, pm):
+
+        # Fetching class data
+        delta_alpha, delta_ell, n, T, alpha, ell = self.delta_alpha, self.delta_ell, self.n, self.T, self.alpha, self.ell
+        A_dot_A, A_dot_B, B_dot_B = self.A_dot_A, self.A_dot_B, self.B_dot_B
+        
+        beta = 1 - alpha
+        
+        M_dot_A = (alpha*A_dot_A + beta*A_dot_B)
+        M_dot_B = (alpha*A_dot_B + beta*B_dot_B)
+        
+        
+        d_M_dot_A_d_alpha = (A_dot_A - A_dot_B)
+        d_M_dot_B_d_alpha = (A_dot_B - B_dot_B)
+        
+        
+        l_A_o_A = np.tanh(ell * ( M_dot_A/T)**n )
+        l_gamma_o_A = -np.tanh(( M_dot_A/T)**n )
+        
+        l_A_o_B = np.tanh(ell * ( M_dot_B/T)**n )
+        l_gamma_o_B = -np.tanh(( M_dot_B/T)**n )
+        
+        
+        nabla_ell_pm = ( (1 - l_A_o_A)**(2*n - 1)   *   (1 - l_A_o_A**2) * M_dot_A**n
+                         
+                         - (1 + l_A_o_B)**(2*n - 1)   *   (1 - l_A_o_B**2) * M_dot_B**n
+                         
+                         + pm * n * (1 - l_A_o_A)**(2*n - 1)   *   (1 - l_A_o_A**2) * M_dot_A**(n-1) * d_M_dot_A_d_alpha * delta_alpha
+                         
+                         - pm * n * (1 + l_A_o_B)**(2*n - 1)   *   (1 - l_A_o_B**2) * M_dot_B**(n-1) * d_M_dot_B_d_alpha * delta_alpha
+                         
+        )
+        
+        return nabla_ell_pm
+    
+
+    def calc_nabla_gamma(self):
+
+        # Fetching class data
+        n, T, alpha, ell = self.n, self.T, self.alpha, self.ell
+        A_dot_A, A_dot_B, B_dot_B = self.A_dot_A, self.A_dot_B, self.B_dot_B
+        
+        beta = 1 - alpha
+        
+        M_dot_A = (alpha*A_dot_A + beta*A_dot_B)
+        M_dot_B = (alpha*A_dot_B + beta*B_dot_B)
+        
+        l_gamma_o_A = -np.tanh(( M_dot_A/T)**n )
+        l_gamma_o_B = -np.tanh(( M_dot_B/T)**n )
+        
+        
+        nabla_gamma = ( - (1 + l_gamma_o_A)**(2*n - 1)  *  (1 - l_gamma_o_A**2) * M_dot_A**n
+                        - (1 + l_gamma_o_B)**(2*n - 1)  *  (1 - l_gamma_o_B**2) * M_dot_B**n
+        )
+        
+
+        return nabla_gamma
+
+
+
+
+    def calc_d_alpha_pm_delta_alpha_dt(self, pm):
+
+        # Fetching class data
+        delta_alpha, delta_ell, n, T, alpha, ell = self.delta_alpha, self.delta_ell, self.n, self.T, self.alpha, self.ell
+        A_dot_A, A_dot_B, B_dot_B = self.A_dot_A, self.A_dot_B, self.B_dot_B
+        
+        nabla_A_pm = self.calc_nabla_A(pm)
+        nabla_B_pm = self.calc_nabla_B(pm)
+        
+        nabla_A = self.calc_nabla_A(0)
+        nabla_B = self.calc_nabla_B(0)
+        
+        d_alpha_pm_delta_alpha_dt = (  nabla_A_pm - alpha*(nabla_A_pm + nabla_B_pm) - pm * delta_alpha * (nabla_A + nabla_B)  ) / ( np.abs(nabla_A_pm) + np.abs(nabla_B_pm) )
+        
+        return d_alpha_pm_delta_alpha_dt
+
+
+    def calc_d_ell_pm_delta_ell_dt(self, pm):
+
+        # Fetching class data
+        delta_alpha, delta_ell, n, T, alpha, ell = self.delta_alpha, self.delta_ell, self.n, self.T, self.alpha, self.ell
+        A_dot_A, A_dot_B, B_dot_B = self.A_dot_A, self.A_dot_B, self.B_dot_B
+
+        
+        nabla_ell_pm = self.calc_nabla_ell(pm)
+        nabla_gamma = self.calc_nabla_gamma()
+        
+        d_ell_pm_delta_ell_dt = nabla_ell_pm / np.maximum( np.abs(nabla_ell_pm), np.abs(nabla_gamma) )
+        
+        return d_ell_pm_delta_ell_dt
+    
+
+
