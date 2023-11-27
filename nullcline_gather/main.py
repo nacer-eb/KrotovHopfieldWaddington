Slight fix to adapt the example code to the new GNC calc_d_alpha_dt output, this now handles normalization conditions

nullcline_gather/main.py
@@ -11,9 +11,10 @@ sample_range = np.linspace(0, 1, 1000)
 l_0_mesh, alpha_mesh = np.meshgrid(sample_range*2-1, sample_range)
 
 
+dt_alpha, norm_condition = GNC.calc_d_alpha_dt(alpha_mesh, l_0_mesh)
 dt_ell, dt_ell_sat_p, dt_ell_sat_m = GNC.calc_d_ell_dt(alpha_mesh, l_0_mesh)
 
-plt.contour(l_0_mesh, alpha_mesh, GNC.calc_d_alpha_dt(alpha_mesh, l_0_mesh), [0], colors="purple", linewidths=3, alpha=0.5)
+plt.contour(l_0_mesh, alpha_mesh, dt_alpha, [0], colors="purple", linewidths=3, alpha=0.5)
 
 plt.contour(l_0_mesh, alpha_mesh, dt_ell, [0], colors="orange", linewidths=3, alpha=0.5)
 plt.contour(l_0_mesh, alpha_mesh, dt_ell_sat_p, [0], colors="orange", linewidths=3, alpha=0.5)