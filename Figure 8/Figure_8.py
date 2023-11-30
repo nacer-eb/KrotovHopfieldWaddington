c726131119f33e878ef91d0663ec8258775232d9
nacer-eb <nacer.boukacem@mail.mcgill.ca>
2023-11-30 15:32:31 -0500

    Adding comments to figure 8 for clarity

--- Figure 8/Figure_8.py
+++ Figure 8/Figure_8.py
@@ -16,8 +16,6 @@ import matplotlib.animation as anim
 
 data_dir = "data/"
 
-isFirstRun = False
-
 selected_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]#
 prefix = str(selected_digits)+"/" # I used main,and momentum #"main"#
 
@@ -38,8 +36,8 @@ data_T = np.load(dataset)[0]
 data_T_inv = np.linalg.pinv(data_T)
 
 
-
-if isFirstRun:
+isFirstRun = True # Set this to False if you've already preloaded the data
+if isFirstRun: # Then preload the data for the first time
     for h, temp in enumerate(temp_range):
         print(n, temp)
     
@@ -55,6 +53,7 @@ if isFirstRun:
     np.save(data_dir + prefix + "data_Ls_T.npy", data_Ls)
     np.save(data_dir + prefix + "data_Coefs_T.npy", data_coefs)
 
+# Fetches preloaded data
 data_Ms = np.load(data_dir + prefix + "data_Ms_T.npy")
 data_Ls = np.load(data_dir + prefix + "data_Ls_T.npy")
 data_coefs = np.load(data_dir + prefix + "data_Coefs_T.npy")
@@ -124,20 +123,26 @@ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAX
 props = dict(boxstyle='round', facecolor='whitesmoke', alpha=0.5)
 t_samples = np.asarray([0, 25, 40, 46, 55])
 
+# The d here is a time/epoch index to plot samples at specific points in training interesting
 for d in range(5):
     t_sample = t_samples[d]
     indices = np.zeros(20)
+
+    # Fetch the number of memories per class
     for i in range(len(indices)):
         strictness = 1
-        all_indices = np.argwhere(data_Ls[t_sample, :, i//2] >= strictness )
+        all_indices = np.argwhere(data_Ls[t_sample, :, i//2] >= strictness ) # Fetch index of all memories in digit class i//2
+
+        # If you don't find any at first loosen the conditions a bit..
         while len(all_indices) == 0:
             strictness -= 0.05
-            all_indices = np.argwhere(data_Ls[t_sample, :, i//2] >= strictness ) #np.argmax(data_Ls[t_sample, 0, :], axis=-1) == i//2 #
+            all_indices = np.argwhere(data_Ls[t_sample, :, i//2] >= strictness ) 
 
+            # Below a certain value look for saddles instead (This is particularly valid for high enough n - the movie doesn't need this step of course)
             if strictness < 0.5:
-                all_indices = np.sum(data_Ls[t_sample, :], axis=-1) <= -8.8 #np.argmax(data_Ls[t_sample, 0, :], axis=-1) == i//2 #
-                
-            
+                all_indices = np.sum(data_Ls[t_sample, :], axis=-1) <= -8.8
+        
+        
         indices[i] =  all_indices[np.random.randint(len(all_indices))] # -> Pick randomly when Label is mostly # digit class i//2 $
     indices = np.asarray(indices, dtype=int)
     
