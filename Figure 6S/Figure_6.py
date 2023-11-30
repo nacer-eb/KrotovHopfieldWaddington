587cccc3f419896dc8f48c2f4a4c2cedd672af02
nacer-eb <nacer.boukacem@mail.mcgill.ca>
2023-11-30 16:53:16 -0500

    Added comments and clarified the code a bit

--- Figure 6S/Figure_6.py
+++ Figure 6S/Figure_6.py
@@ -112,15 +112,7 @@ axs['A'].set_xticks([]); axs['A'].set_yticks([])
 axs['B'].set_xticks([]); axs['B'].set_yticks([])
 
 
-"""
 
-ax_cb_mem = axs['$']
-bwr_cmap = matplotlib.cm.bwr
-bwr_norm = matplotlib.colors.Normalize(vmin=-1, vmax=1)
-cb_mem = matplotlib.colorbar.ColorbarBase(ax_cb_mem, cmap=bwr_cmap, norm=bwr_norm, orientation='vertical')
-cb_mem.set_label("Pixel value")
-
-"""
 
 #########################################
 #########################################
@@ -133,7 +125,7 @@ n_range = np.arange(2, 31, 1)
 
 data_Ms_intra = np.zeros((len(temp_range), len(n_range), 1, 784))
 
-first_run = True
+first_run = True # True needs to be run at least once, False makes things a bit faster for re-runs
 if first_run:
     for i, temp in enumerate(temp_range):
         for j, n in enumerate(n_range):
@@ -142,18 +134,25 @@ if first_run:
             data = np.load(saving_dir)
             data_Ms_intra[i, j] = data['M']
     
-        print(i)
+    # Save preloaded data
     np.save(data_dir_intra + "data_Ms.npy", data_Ms_intra)
+
+# Load preloaded data
 data_Ms_intra = np.load(data_dir_intra + "data_Ms.npy")
 
+# Get coefficients from memory
 data_coefs_intra = data_Ms_intra @ data_T_intra_inv
-    
+
+# Set aspect ratio of memory sample plot
 aspect = (np.min(n_range) - np.max(n_range))/(np.min(temp_range_rescaled) - np.max(temp_range_rescaled))
 extent = [np.min(n_range), np.max(n_range), np.max(temp_range_rescaled), np.min(temp_range_rescaled)]
 
+# Define colormap for futur use
 tab10_cmap = matplotlib.colormaps["tab10"]
 tab10_norm = matplotlib.colors.Normalize(0, 10)
 
+
+# Used for 1-memory intradigit ; white -> color -> black
 def get_custom_cmap(digit_class):
     custom_cmap = np.zeros((256, 4))
     colors = np.asarray([np.asarray([1.0, 1.0, 1.0, 1.0]),  np.asarray(tab10_cmap(tab10_norm(digit_class))), np.asarray([0.0, 0.0, 0.0, 1.0])])
@@ -166,6 +165,8 @@ def get_custom_cmap(digit_class):
     
     return custom_cmap
 
+
+# Used for one of the 2-memories in the interdigit case ; color -> white
 def get_custom_cmap_2(digit_class):
     custom_cmap = np.zeros((256, 4))
     colors = np.asarray([np.asarray(tab10_cmap(tab10_norm(digit_class))), np.asarray([1.0, 1.0, 1.0, 1.0])])
@@ -177,6 +178,7 @@ def get_custom_cmap_2(digit_class):
     
     return custom_cmap
 
+# Used for one of the 2-memories in the interdigit case ; white -> color
 def get_custom_cmap_2_inv(digit_class):
     custom_cmap = np.zeros((256, 4))
     colors = np.asarray([np.asarray([1.0, 1.0, 1.0, 1.0]), np.asarray(tab10_cmap(tab10_norm(digit_class)))])
@@ -188,28 +190,20 @@ def get_custom_cmap_2_inv(digit_class):
     
     return custom_cmap
 
-def get_custom_cmap_3(digit_class, digit_class_2):
-    custom_cmap = np.zeros((256, 4))
-    colors = np.asarray([np.asarray(tab10_cmap(tab10_norm(digit_class))), np.asarray([1.0, 1.0, 1.0, 1.0]), np.asarray(tab10_cmap(tab10_norm(digit_class_2)))])
-    
-    x = np.linspace(0, 1, 128)
-    for i in range(128):
-        custom_cmap[i] = x[i]*colors[1] + (1.0-x[i])*colors[0]
-        custom_cmap[i+128] = x[i]*colors[2] + (1.0-x[i])*colors[1]
-    custom_cmap = matplotlib.colors.ListedColormap(custom_cmap)
-    
-    return custom_cmap
 
-
+# Plotting memory samples
 ax = np.asarray([axs['C'], axs['D']])
 ax[0].imshow(merge_data(data_Ms_intra[::4, ::4, 0, :].reshape(len(n_range[::4])*len(temp_range[::4]), 784), len(n_range[::4]), len(temp_range[::4])  ),
              cmap="bwr", vmin=-1, vmax=1, extent=extent, aspect=aspect)
 ax[0].set_xlabel(r"$n$")
 ax[0].set_ylabel(r"Rescaled Temperature", labelpad=20)
-    
-cmap_ortho = get_custom_cmap(digit_classes[0]) # By default
-data_ortho = data_coefs_intra[:, :, 0, 0] # <<<<<<<<<<<<----- MAKE THE CHOICE OF INDEX AUTOMATIC!!!!
+
+
+cmap_ortho = get_custom_cmap(digit_classes[0]) # By default I assume that your intradigit class is the first class of your interdigit class
+data_ortho = data_coefs_intra[:, :, 0, 0] # You need to pick the dominating digit at high n, so this can also be [:, :, 0, 1] 
 norm_ortho = matplotlib.colors.Normalize(0.4, 1)
+
+# Plot coefficient of the dominating digit (it shows intradigitclass interaction best considering the colormap I used)
 ax[1].imshow(data_ortho, cmap=cmap_ortho, norm=norm_ortho, extent=extent, aspect=aspect) #
 ax[1].set_xlabel(r"$n$")
 ax[1].set_ylabel(r"Rescaled Temperature", labelpad=20)
@@ -218,7 +212,7 @@ ax[1].set_ylabel(r"Rescaled Temperature", labelpad=20)
 
 
 
-# Plotting slices
+# Plotting slices for temperatures
 colors=["red", "orange", "blue"]
 for t_i, t in enumerate([-1, -3, -5]):
     ax[1].axhline(y=temp_range_rescaled[t]-0.004, xmax=1, color=colors[t_i], linewidth=10, label=r"$T_r$ = "+'{0:.2f}'.format(temp_range_rescaled[t]))
@@ -231,7 +225,7 @@ for t_i, t in enumerate([-1, -3, -5]):
 
 import matplotlib.patheffects as pe
 
-# Plotting the theoretical prediction (see appendix)
+# Plotting the theoretical prediction (see supplemental material)
 n = np.arange(np.min(n_range), np.max(n_range), 0.01)
 T_calc = (data_T_intra[0]@data_T_intra[0] + data_T_intra[0]@data_T_intra[1])/( 2 * ( np.arctanh( 1 - (1.0/2.0)**(1.0/(2.0*n)) ) )**(1.0/n) )
 T_calc_rescaled = T_calc/784.0
@@ -239,6 +233,7 @@ ax[1].plot(n, T_calc_rescaled, color='white', linewidth=10, path_effects=[pe.Str
 ax[1].set_ylim(max(temp_range_rescaled), min(temp_range_rescaled))
 
 
+# Plotting colorbar
 ax_cb_intra = axs['d']
 cb_bwr = matplotlib.colorbar.ColorbarBase(ax_cb_intra, cmap=cmap_ortho, norm=norm_ortho, orientation='horizontal')
 cb_bwr.set_label(r"$\alpha_{"+str(digit_classes[0])+", 1}$", labelpad=40)
@@ -261,7 +256,7 @@ temp_range_rescaled = temp_range/784.0
 n_range = np.arange(2, 31, 1)
 data_Ms_inter = np.zeros((len(temp_range), len(n_range), 2, 784))
 
-first_run = True
+first_run = True # You can set this to False if you've run this code before and have the preloaded data. Minor speed improvement in this case
 if first_run:
     for i, temp in enumerate(temp_range):
         for j, n in enumerate(n_range):
@@ -270,55 +265,65 @@ if first_run:
             data = np.load(saving_dir)
             data_Ms_inter[i, j] = data['M']
     
-        print(i)
+    # Saves preloaded data
     np.save(data_dir_inter + "data_Ms.npy", data_Ms_inter)
+
+# Loads preloaded data
 data_Ms_inter = np.load(data_dir_inter + "data_Ms.npy")
 
+# Obtains coefficient using Moore-Penrose Inverse
 data_coefs_inter = data_Ms_inter @ data_T_inter_inv
 
+# Cosmetics for later plots
 aspect = (np.min(n_range) - np.max(n_range))/(np.min(temp_range_rescaled) - np.max(temp_range_rescaled))
 extent = [np.min(n_range), np.max(n_range), np.max(temp_range_rescaled), np.min(temp_range_rescaled)]
 
+
+# To refer to axis using int indices
 ax = np.asarray([[axs['F'], axs['G']], [axs['I'], axs['J']] ])
 ax_1d = np.asarray([axs['H'], axs['K']])
-
 ax_cb = np.asarray([axs['g'], axs['j']])
 
-# -0.5 to 0.5
 
+# Digit to run through the 2 memories
 for digit in [0, 1]:
+
+    # Plot memory samples per n, T (not all n,T of course otherwise it'd be too small)
     ax[digit, 0].imshow(merge_data(data_Ms_inter[::4, ::4, digit, :].reshape(len(n_range[::4])*len(temp_range[::4]), 784), len(n_range[::4]), len(temp_range[::4])  ),
              cmap="bwr", vmin=-1, vmax=1, extent=extent, aspect=aspect)
 
 
-    cmap_ortho = get_custom_cmap_2(digit_classes[digit]) # get_custom_cmap(digit_classes[1-digit])
-    data_ortho = data_coefs_inter[:, :, digit, 1-digit]
+    # Obtain color map
+    cmap_ortho = get_custom_cmap_2(digit_classes[digit])
+    data_ortho = data_coefs_inter[:, :, digit, 1-digit] # Pick the coefficient of the minor digit, this shows interdigit interaction best
 
-    if digit == 0:
-        norm_ortho = matplotlib.colors.Normalize(vmin=-0.5, vmax=0.5)
-    if digit == 1:
-        norm_ortho = matplotlib.colors.Normalize(vmin=-0.5, vmax=0.5)
     
-    im = ax[digit, 1].imshow(data_ortho, cmap=cmap_ortho, norm=norm_ortho, extent=extent, aspect=aspect)
+    norm_ortho = matplotlib.colors.Normalize(vmin=-0.5, vmax=0.5)
+    
+    
+    im = ax[digit, 1].imshow(data_ortho, cmap=cmap_ortho, norm=norm_ortho, extent=extent, aspect=aspect) # Plot minor digit coefficient
 
+
+    # Calc prediction (see Supplementary material)
     n = np.arange(np.min(n_range), np.max(n_range), 0.01)
 
     # I used 1-digit here to deal with potential asymmetry, but it shouldn't matter A@A should be roughly B@B
     T_calc = (data_T_inter[1-digit]@data_T_inter[1-digit] + data_T_inter[0]@data_T_inter[1])/( 2 * ( np.arctanh( 1 - (1.0/2.0)**(1.0/(2.0*n)) ) )**(1.0/n) )
     T_calc_rescaled = T_calc/784.0
     
-    
+
+    # Cosmetics
     ax[digit, 1].set_ylim(max(temp_range_rescaled), min(temp_range_rescaled))
 
     ax[1, 0].set_yticks([])
     ax[1, 1].set_yticks([])
     
-    
     ax[digit, 0].set_xlabel(r"$n$", labelpad=10); ax[digit, 1].set_xlabel(r"$n$", labelpad=10)
     ax[0, 0].set_ylabel("Rescaled Temperature", labelpad=20)
     ax[0, 1].set_ylabel("Rescaled Temperature", labelpad=20)
     
-    
+
+    # Plotting colorbar
     cb_bwr = matplotlib.colorbar.ColorbarBase(ax_cb[digit], cmap=cmap_ortho, norm=norm_ortho, orientation='horizontal')
     cb_bwr.set_label(r"$\alpha_" + str(digit_classes[1-digit]) + "$", labelpad=40)
     ax_cb[digit].xaxis.set_ticks_position('top')
@@ -326,6 +331,7 @@ for digit in [0, 1]:
     cb_bwr.set_ticks([-0.5, 0, 0.5]) # Finally found how to center these things 
     cb_bwr.set_ticklabels([-0.5, 0, 0.5])
 
+    # Plotting temperature slices
     colors=["red", "orange", "blue"]
     for t_i, t in enumerate([-1, -5, -8]):
         ax[digit, 1].axhline(y=temp_range_rescaled[t]-0.004, xmax=1, color=colors[t_i], linewidth=10, label=r"$T_r$ = "+'{0:.2f}'.format(temp_range_rescaled[t]))
@@ -340,10 +346,12 @@ for digit in [0, 1]:
     ax_1d[1].set_yticks([])
         
 
+    # Plot prediction
     ax[digit, 1].plot(n, T_calc_rescaled, color='white', linewidth=10, path_effects=[pe.Stroke(linewidth=15, foreground='k'), pe.Normal()], label="Theoretical line")
     ax[digit, 1].legend(bbox_to_anchor=(1, -1.6))
 
 
+# Special inverted colorbar for one of the coefficient plots 
 cmap_ortho = get_custom_cmap_2_inv(digit_classes[1])
 cb_bwr = matplotlib.colorbar.ColorbarBase(ax_cb[1], cmap=cmap_ortho, norm=norm_ortho, orientation='horizontal')
 cb_bwr.set_label(r"$\alpha_" + str(digit_classes[1-digit]) + "$", labelpad=40)
